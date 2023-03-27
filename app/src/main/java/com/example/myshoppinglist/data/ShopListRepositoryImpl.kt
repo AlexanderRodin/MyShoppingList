8c86267b1e7a6645db2b9b318e08c9a297244488
alexanderrodin <pass.9157@gmail.com>
2023-03-27 19:35:04 +0300

    list output with LinearLayout

--- app/src/main/java/com/example/myshoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/myshoppinglist/data/ShopListRepositoryImpl.kt
@@ -8,11 +8,11 @@ import com.example.myshoppinglist.domain.ShopListRepository
 object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ p0, p1 -> p0.id.compareTo(p1.id) })
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 10){
+        for (i in 0 until 10000){
             val item = ShopItem("Name $i", i, true)
             addShopItem(item)
         }
@@ -35,7 +35,7 @@ object ShopListRepositoryImpl : ShopListRepository {
         val oldElement = getShopItem(shopItem.id)
         shopList.remove(oldElement)
         addShopItem(shopItem)
-    }
+        }
 
     @Throws(RuntimeException::class)
     override fun getShopItem(shopItemId: Int): ShopItem {
