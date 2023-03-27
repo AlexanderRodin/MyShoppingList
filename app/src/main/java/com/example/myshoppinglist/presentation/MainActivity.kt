5c98896e5f0efb11ee220fec0aa535fec2bfed30
alexanderrodin <pass.9157@gmail.com>
2023-03-27 17:08:30 +0300

    added home screen layout

--- app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.myshoppinglist.R
 
 class MainActivity : AppCompatActivity() {
 
+    private var count = 0
     private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +16,11 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
             Log.d("MainViewModel", it.toString())
+            if (count == 0){
+                count++
+                val item = it[0]
+                viewModel.changeEnabledState(item)
+            }
         }
-        viewModel.getShopList()
     }
 }
